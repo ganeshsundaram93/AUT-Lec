0b02f726cd14424b336d1fdfae3325c60856e124
Ganesh Sundaram <ganeshs93@gmail.com>
2017-12-03 23:53:59 +0100

    updated

--- SSP/sensig_lab2/SensigLab_Python_2017/conwav2npy.py
+++
@@ -1,6 +0,0 @@
-from scipy.io import wavfile
-import numpy as np
-
-sample_rate, wave_data = wavfile.read("2_euro_1.wav")
-wave_data = wave_data.reshape((-1,1)) # -1 keeps the same dimension shape
-np.save("2_euro_1.npy", wave_data)
