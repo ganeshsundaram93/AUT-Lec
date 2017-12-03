596b4aae04c677373de95ae9ae9c2339fd46728d
Ganesh Sundaram <ganeshs93@gmail.com>
2017-12-03 23:48:19 +0100

    updated

---
+++ SSP/Sensig2017_lab3/sel_diff_freq.py
@@ -0,0 +1,51 @@
+import numpy as np
+import scipy
+import pylab
+
+magspecdata= np.load("glass_spec.npy")
+
+x,y = magspecdata.shape
+Fs=44100
+
+
+def plotall(ydata,y):
+
+    #print "Dim:", x
+    vert = y/5
+    hor = y/2
+    import pylab 
+    for i in range (0, hor):
+        for j in range (0, vert):
+            pylab.subplot(vert, hor, i+j*hor+1)
+            pylab.plot(ydata[:,i+j*hor])
+    pylab.show()
+    print "Plot of the preprocessed sound data:"
+
+for i in range (0, 5):
+    if i==0: 
+        Spec_Class2 = magspecdata[:,0]
+        Spec_Class1 = magspecdata[:,5]
+    else:
+        Spec_Class2 = Spec_Class1 + magspecdata[:,i]
+        Spec_Class1 = Spec_Class1 + magspecdata[:,i+5]
+
+Spec_Diff = abs(Spec_Class1-Spec_Class2)/5
+pylab.plot(Spec_Diff)
+pylab.show()
+
+plotall(magspecdata,y)
+
+for i in range (0, 10):
+    magspecdata[:,i]=magspecdata[:,i]*Spec_Diff 
+
+plotall(magspecdata,y)
+np.save('glass_spec_sel.npy',magspecdata)
+
+import py2nif as p2n
+p2n.py2nif(magspecdata,"Glas_MagSpec_Sel.nif")
+#class_labels = np.array([2,2,2,2,2,1,1,1,1,1])
+#class_labels= np.reshape(class_labels,(1,class_labels.shape[0]))
+#print "class labels:", class_labels
+#p2n.py2nif(class_labels,"Glas_MagSpec_Class.nif")
+
+
